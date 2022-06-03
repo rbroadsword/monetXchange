85cda99e260fdee3248572d30b5f8974a9547b62
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-02 20:21:05 -0700

    add Monet class in function.js, assign API_KEY

--- src/js/function.js
+++ src/js/function.js
@@ -1,1 +1,14 @@
-//for business logic, constructors, and functions
+export default class Monet {
+  static getMonet(country, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText); 
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error; 
+      })
+  }
+}
