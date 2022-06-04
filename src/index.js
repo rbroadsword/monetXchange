db5035330e5055db940263316183b60692e4a71b
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-03 17:12:14 -0700

    import class Monet and write getElements()

--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Monet from './js/function.js';
 
+function getElements(response){
+  if (response.result === "success") {
+    $('.conversionRate').text(`The coversion rate is: ${response.conversion_rate}`); 
+    $('.conversionResult').text(`Conversion result from USD to ${country} is: ${response.conversion_result}`)
+  } else {
+    $('.error')
+  }
+} 
 
 $(document).ready(function() {
   $('.monetForm').click(function() {
     event.preventDefault(); 
-    let coin = $('#currency').val(); 
+    let country = $('#currency').val(); 
     let amount = parseInt($('#amount').val()); 
-    console.log("Coin selected is " + coin); 
-    console.log("Amount: " + amount); 
+    console.log("Coin selected is " + country); 
+    console.log("Amount: " + amount);
+    Monet.getMonet(country, amount)
+      .then(function(response) {
+        getElements(response);
+      });
   });
 }); 
